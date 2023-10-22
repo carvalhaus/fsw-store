dfbd8ea4b676880bb5be72c9c100c67c4e607591
carvalhaus <ooliveira.joaop@gmail.com>
2023-10-22 00:38:17 -0300

    feat: add link in CategoryItem Component to redirect do CategoryProduct Page

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,6 +1,7 @@
 import { CATEGORY_ICON } from "@/app/constants/category-icon";
 import { Badge } from "@/components/ui/badge";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -8,13 +9,15 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <Badge
-      variant="outline"
-      className="py-3 flex justify-center items-center gap-3 rounded-lg"
-    >
-      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-      <span className="text-xs font-bold">{category.name}</span>
-    </Badge>
+    <Link href={`category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="py-3 flex justify-center items-center gap-3 rounded-lg"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
