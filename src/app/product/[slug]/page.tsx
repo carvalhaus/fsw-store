5d2b5708578e1b67858db1e9f904c3ad0e5160fa
carvalhaus <ooliveira.joaop@gmail.com>
2023-10-22 10:00:23 -0300

    feat: add Link in Product Item to redirect to ProductDetialsPage

---
+++ src/app/product/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import { prismaClient } from "@/lib/prisma";
+
+interface ProductDetailPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const ProductDetailPage = async ({
+  params: { slug },
+}: ProductDetailPageProps) => {
+  const product = await prismaClient.product.findFirst({
+    where: {
+      slug: slug,
+    },
+  });
+
+  if (!product) {
+    return null;
+  }
+
+  return <h1>{product.name}</h1>;
+};
+
+export default ProductDetailPage;
