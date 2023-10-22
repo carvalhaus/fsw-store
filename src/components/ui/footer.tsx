cef6e3c573a860bc01a9a862065235e356113f2c
carvalhaus <ooliveira.joaop@gmail.com>
2023-10-21 21:46:08 -0300

    feat: add Footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="px-8 py-5 bg-neutral-900">
+      <p className="text-xs text-neutral-600">
+        © 2023 Copyright <span className="font-bold">FSW Store</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
