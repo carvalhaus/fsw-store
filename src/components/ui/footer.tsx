98bb4ea38145f6296cadc2b92619f51723241cf3
carvalhaus <ooliveira.joaop@gmail.com>
2023-10-21 22:14:47 -0300

    fix: add margin top Footer component

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="px-8 py-5 bg-neutral-900">
+    <footer className="px-8 py-5 bg-neutral-900 mt-5">
       <p className="text-xs text-neutral-600">
         © 2023 Copyright <span className="font-bold">FSW Store</span>
       </p>
