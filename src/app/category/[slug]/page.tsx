3831c145196856660254fba345290c6cf091d6a0
carvalhaus <ooliveira.joaop@gmail.com>
2023-10-22 00:32:55 -0300

    feat: add categories pages

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,5 +1,43 @@
-const CategoryProducts = ({ params }: any) => {
-  return <h1>{params.slug}</h1>;
+import { CATEGORY_ICON } from "@/app/constants/category-icon";
+import { Badge } from "@/components/ui/badge";
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+
+const CategoryProducts = async ({ params }: any) => {
+  const category = await prismaClient.category.findFirst({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      products: true,
+    },
+  });
+
+  if (!category) {
+    return null;
+  }
+
+  return (
+    <section className="p-5">
+      <Badge
+        variant={"outline"}
+        className="border-primary gap-1 uppercase font-bold"
+      >
+        {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
+        {category.name}
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-4 mt-5">
+        {category.products.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrice(product)}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CategoryProducts;
