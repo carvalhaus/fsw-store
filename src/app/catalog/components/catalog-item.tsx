444e0f4b1e57287bbf36b204c2ee7f95bf9454e9
carvalhaus <ooliveira.joaop@gmail.com>
2023-10-21 23:57:09 -0300

    feat: add Link in CatalogItem to redirect to category slug page

--- src/app/catalog/components/catalog-item.tsx
+++ src/app/catalog/components/catalog-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -7,22 +8,24 @@ interface CategoryItemProps {
 
 const CatalogItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-center items-center w-full h-[150px] rounded-t-lg bg-category-item-gradient">
-        <Image
-          src={category.imageUrl}
-          alt={category.slug}
-          key={category.id}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-auto h-auto max-w-[80%] max-h-[70%]"
-        />
+    <Link href={`/category/${category.slug}`}>
+      <div className="flex flex-col">
+        <div className="flex justify-center items-center w-full h-[150px] rounded-t-lg bg-category-item-gradient">
+          <Image
+            src={category.imageUrl}
+            alt={category.slug}
+            key={category.id}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-auto h-auto max-w-[80%] max-h-[70%]"
+          />
+        </div>
+        <p className="bg-neutral-900 w-[100%] text-center rounded-b-lg py-2 font-bold">
+          {category.name}
+        </p>
       </div>
-      <p className="bg-neutral-900 w-[100%] text-center rounded-b-lg py-2 font-bold">
-        {category.name}
-      </p>
-    </div>
+    </Link>
   );
 };
 
---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const CategoryProducts = ({ params }: any) => {
+  return <h1>{params.slug}</h1>;
+};
+
+export default CategoryProducts;
