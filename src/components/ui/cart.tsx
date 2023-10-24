500623f271e32b8f517aa0ab843f8e8349344039
carvalhaus <ooliveira.joaop@gmail.com>
2023-10-24 06:45:26 -0300

    feat: add Sheet, Sheet trigger and Sheet content in Header

---
+++ src/components/ui/cart.tsx
@@ -0,0 +1,26 @@
+import { ShoppingCartIcon } from "lucide-react";
+import { Badge } from "./badge";
+import { useContext } from "react";
+import { CartContext } from "@/providers/cart";
+
+const Cart = () => {
+  const { products } = useContext(CartContext);
+
+  return (
+    <div>
+      <Badge
+        variant={"outline"}
+        className="border-primary gap-1 uppercase font-bold"
+      >
+        <ShoppingCartIcon size={16} />
+        Carinho
+      </Badge>
+
+      {products.map((product) => (
+        <h1 key={product.id}>{product.name}</h1>
+      ))}
+    </div>
+  );
+};
+
+export default Cart;
