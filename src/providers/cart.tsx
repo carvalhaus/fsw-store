60b6360a71a9759cef4594b2106e41da15bac558
carvalhaus <ooliveira.joaop@gmail.com>
2023-10-24 22:03:28 -0300

    feat: add functionality do increase the quantity of ProductItem in the CartItem

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -20,6 +20,7 @@ interface ICartContext {
   cartTotalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -29,6 +30,7 @@ export const CartContext = createContext<ICartContext>({
   cartTotalPrice: 0,
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
 });
 
 const CartContextProvider = ({ children }: { children: ReactNode }) => {
@@ -74,12 +76,28 @@ const CartContextProvider = ({ children }: { children: ReactNode }) => {
     );
   };
 
+  const increaseProductQuantity = (productId: string) => {
+    setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+
+        return cartProduct;
+      })
+    );
+  };
+
   return (
     <CartContext.Provider
       value={{
         products,
         addProductToCart,
         decreaseProductQuantity,
+        increaseProductQuantity,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
         cartTotalPrice: 0,
